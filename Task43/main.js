ecdc3c38691af85463655bee184e7a1bfc98313b
hungdeptrai <trinhquochung2611204@gmail.com>
2025-05-13 08:43:14 +0700

    update

--- Task43/main.js
+++ Task43/main.js
@@ -32,9 +32,7 @@ function renderProducts() {
     )
     .join('');
 
-  document.getElementById(
-    'productCount'
-  ).innerText = `Hiển thị ${paginated.length} / ${allProducts.length} sản phẩm`;
+  document.getElementById('productCount').innerText = ``;
   renderPagination();
 }
 
